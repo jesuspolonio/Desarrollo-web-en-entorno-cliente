f89db58f8c0efb260bb8a96e83435853b1d11293
Jesús Polonio Castejón <92536817+jesuspolonio@users.noreply.github.com>
2021-10-21 17:21:04 +0200

    Delete dwes21 directory

--- dwes21/Tema 3/ejer9.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $semanaSiguiente = time() + (7 * 24 * 60 * 60);
-
-    echo "Ahora:             ".date('Y-m-d')."<br/>";
-    echo "Semana siguiente:  ".date('Y-m-d', $semanaSiguiente)."<br/>";
-    echo "Semana siguiente:  ".date('Y-m-d', strtotime('+ 1 week'))."<br/>";
-
-?>
